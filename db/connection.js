9da263ed031ecc1838855022d6886f972a980352
ONyeNYC <dis2isaac@yahoo.com>
2023-11-02 10:53:55 -0400

    add heroku

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
 
+let MONGODB_URI =
+  process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/api-project";
+
 //configuring a Mongoose option to control the behavior of document updates.
 mongoose.set("returnOriginal", false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/api-project").catch(err => {
+mongoose.connect(MONGODB_URI).catch(err => {
+  // mongodb+srv://dis2isaac:<password>@cluster0.knjlhgs.mongodb.net/?retryWrites=true&w=majority
   console.log(`Error connection go MongoDB: ${err.message}`);
 });
 
